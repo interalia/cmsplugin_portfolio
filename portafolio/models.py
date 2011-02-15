6f0eab1e74977d728809837857fa7ae6d75808fa
Andres Vargas <andres.vargas@interalia.net>
2011-02-14 19:13:26 -0600

    update plugin

--- portafolio/models.py
+++ portafolio/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django_countries import CountryField
 from cms.models import CMSPlugin
 from django.core.exceptions import ValidationError
 
--- portafolio/urls.py
+++ portafolio/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('portafolio.views',
-    url(r"^$", "index", name="portafolio-index"),
-    url(r"^small/$", "show_small", name="portafolio-small"),
-    url(r"^redirect/$", "redirect", name="portafolio-redirect"),
-    url(r"^detail/(?P<slug>[-\w]+)/$", "proyect", name="portafolio-proyect"),
+urlpatterns = patterns('',
+    url(r"^$", "portafolio.views.index", name="portafolio-index"),
+    url(r"^redirect/$", "portafolio.views.redirect", name="portafolio-redirect"),
+    url(r"^detail/(?P<slug>[-\w]+)/$", "portafolio.views.proyect", name="portafolio-proyect"),
 )
-
